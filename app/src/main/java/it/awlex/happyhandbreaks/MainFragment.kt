2d5dab473adfe0cd3cebe6a98025f27826b5552c
Awlex <wieseralex580@gmail.com>
2017-07-27 19:37:58 +0200

    the state of the numberpickers will now be kept after a recreate

--- app/src/main/java/it/awlex/happyhandbreaks/MainFragment.kt
+++ app/src/main/java/it/awlex/happyhandbreaks/MainFragment.kt
@@ -19,21 +19,32 @@ class MainFragment : Fragment() {
          * Mistakes were made
          */
         fun newInstance(): MainFragment = MainFragment()
+        private val BETWEENKEY = "between_key"
+        private val DURATIONKEY = "duration_key"
     }
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
 
+        var betweenValue = 3
+        var durationValue = 1
+
+        if (savedInstanceState != null) {
+            betweenValue = savedInstanceState.getInt(BETWEENKEY)
+            durationValue = savedInstanceState.getInt(DURATIONKEY)
+        }
         // Inflate the layout for this fragment
         v = inflater!!.inflate(R.layout.fragment_main, container, false)
 
         // Temporary number
-        v.numberPicker.maxValue = 10
-        v.numberPicker.displayedValues = Array(v.numberPicker.maxValue + 1, { i -> (i * 10 + 30).toString() })
+        v.between_numberpicker.maxValue = 9
+        v.between_numberpicker.displayedValues = Array(v.between_numberpicker.maxValue + 1, { i -> (i * 10 + 30).toString() })
+        v.between_numberpicker.value = betweenValue
 
         // Temporary number
-        v.numberPicker2.maxValue = 40
-        v.numberPicker2.displayedValues = Array(v.numberPicker2.maxValue + 1, { i -> (i * 5 + 5).toString() })
+        v.duration_numberpicker.maxValue = 39
+        v.duration_numberpicker.displayedValues = Array(v.duration_numberpicker.maxValue + 1, { i -> (i * 5 + 5).toString() })
+        v.duration_numberpicker.value = durationValue
 
         return v
     }
@@ -47,10 +58,19 @@ class MainFragment : Fragment() {
     /**
      * * returns the preferred duration for a break in ms
      */
-    fun getDuration(): Long = v.numberPicker.currentDisplayValueInMillis()
+    fun getDuration(): Long = v.between_numberpicker.currentDisplayValueInMillis()
 
     /**
      * returns the preferred time between breaks in ms
      */
-    fun getBetween(): Long = v.numberPicker2.currentDisplayValueInMillis()
+    fun getBetween(): Long = v.duration_numberpicker.currentDisplayValueInMillis()
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        outState ?: return super.onSaveInstanceState(outState)
+
+        outState.putInt(BETWEENKEY, v.between_numberpicker.value)
+        outState.putInt(DURATIONKEY, v.duration_numberpicker.value)
+        super.onSaveInstanceState(outState)
+    }
+
 }
